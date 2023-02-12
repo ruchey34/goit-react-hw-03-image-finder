e34c95124b40a436425439621dc62eeeedb113e4
ruchey34 <me.oleg1@gmail.com>
2023-02-12 15:29:37 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import ImageGallery from './ImageGallery';
 import Button from './Button/Button';
 import Loader from './Loader';
 
+
 export class App extends Component {
   state = {
     search: '',
